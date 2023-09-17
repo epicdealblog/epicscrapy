b702fb50fdb887e71cc82de5b422483584076a9b
“epicdeal.blog@gmail.com” <epicdeal.us@gmail.com>
2023-09-17 16:42:47 -0700

    Proved that scrapy-splash cannot load nordstrom locally

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -6,16 +6,13 @@ from scrapy_splash import SplashRequest
 # cd /Users/bryanlv/epic/epicdealblog/quotes-js-project/quotes_js_scraper
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    name = 'nordstrom'
+    allowed_domains = ['www.nordstrom.com']
+    start_urls = ["https://www.nordstrom.com/s/vitamin-enriched-face-base-priming-moisturizer-duo-132-value/7396982?origin=category-personalizedsort&breadcrumb=Home%2FAnniversary%20Sale%2FAll%20Anniversary&color=000"]
+    
     def start_requests(self):
-        url = 'https://quotes.toscrape.com/js'
+        url = 'https://www.nordstrom.com/s/vitamin-enriched-face-base-priming-moisturizer-duo-132-value/7396982?origin=category-personalizedsort&breadcrumb=Home%2FAnniversary%20Sale%2FAll%20Anniversary&color=000'
         yield SplashRequest(url, callback=self.parse)
         
     def parse(self, response):
-        quote_item = QuoteItem()
-        for quote in response.css('div.quote'):
-            quote_item['text'] = quote.css('span.text::text').get()
-            
-            yield quote_item
+        print(response.text)
